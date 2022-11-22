12360be2519bfeec8a759c261fe6f5897440de50
Nikola Mitrovic <n.mitrovic@vegait.rs>
2022-11-22 08:07:34 +0100

    After image lazy load.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
-import React from 'react';
-import LazyImg from './components/LazyImg/LazyImg';
-import RocketLoader from './components/RocketLoader/RocketLoader';
-
-import logo from './assets/images/omega.png';
-import galaxy from './assets/images/galaxy.jpeg';
-import galaxyLight from './assets/images/galaxy-light.jpeg';
-import './App.css';
+import React from "react";
+import LazyImg from "./components/LazyImg/LazyImg";
+import RocketLoader from "./components/RocketLoader/RocketLoader";
+import space from "./assets/images/space-background.jpg";
+import astronaut from "./assets/images/astronaut.png";
+import galaxy from "./assets/images/galaxy.jpeg";
+import galaxyLight from "./assets/images/galaxy-light.jpeg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="section section--first">
-        <img src={logo} className="omega__logo" alt="logo" />
-      </div>
-      <div className="section section--second">
-        <RocketLoader />
-      </div>
-      <img src={galaxy} />
-      {/* <LazyImg
-        src={galaxy}
-        fallback={galaxyLight}
-        className="omega__logo"
-        alt="logo"
-      /> */}
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* <img src={galaxy} /> */}
+			<LazyImg src={galaxy} fallback={galaxyLight} alt="logo" />
+			<div className="section section-first">
+				<img src={space} alt="logo" />
+				<img src={astronaut} className="astronaut" alt="logo" />
+			</div>
+			<div className="section section-second">
+				<RocketLoader />
+			</div>
+		</div>
+	);
 }
 
 export default App;
